dac9eeeb67a9786b15e165db05027b4b4401f240
Steven Mather <stevenrmather@gmail.com>
2021-09-24 14:33:39 -0700

    completeing add two numbers

--- LinkedListsTraining/LinkedListTraining_tests/AddTwoNumbers_tests.cs
+++ LinkedListsTraining/LinkedListTraining_tests/AddTwoNumbers_tests.cs
@@ -33,5 +33,138 @@ namespace LinkedListTraining_tests
 
             AssertLinkedLists.NodesHaveEqualValue(expected, actual);
         }
+        [TestMethod]
+        public void AddTwoNumbers_L1Longer_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 1, 1, 1 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 2 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 3, 3, 1 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_L2Longer_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 1, 1 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 2, 2 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 3, 3, 2 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_L2WayLonger_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 1, 1 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 2, 2,2,2,2,2,2,2,2,2,2 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 3, 3, 2,2,2,2,2,2,2,2,2,2 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_AddZero_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 1, 1 ,1 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 2, 0 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 3, 3, 1 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_AddBothZero_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 0 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 0 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 0 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_SimpleCarry_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 8, 2 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 2, 2 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 4, 0, 5 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_MoreSimpleCarry_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 4, 3});
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 5, 6, 4 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 7, 0 ,8 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_CarryAtEnd_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 2, 4, 6 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 5, 2, 4 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 7, 6, 0, 1 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        
+        [TestMethod]
+        public void AddTwoNumbers_Some9s_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 9, 9, 9, 9, 9, 9, 9 } );
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 9, 9, 9, 9 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 8, 9, 9, 9, 0, 0, 0, 1 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_LongLong_ReturnsAddedNumbers()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
+        [TestMethod]
+        public void AddTwoNumbers_WRONG()
+        {
+            var input_l1 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 4 });
+            var input_l2 = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 4 });
+            var expected = LinkedListBuilder.CreateSinglyLinkedList(new int[] { 5 });
+            var sut = new Solution();
+
+            var actual = sut.AddTwoNumbers(input_l1, input_l2);
+
+            AssertLinkedLists.NodesHaveEqualValue(expected, actual);
+        }
     }
 }
--- LinkedListsTraining/LinkedListsTraining/AddTwoNumbers.cs
+++ LinkedListsTraining/LinkedListsTraining/AddTwoNumbers.cs
@@ -1,16 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LinkedListsTraining.AddTwoNumbers
+﻿namespace LinkedListsTraining.AddTwoNumbers
 {
     public class Solution
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            throw new NotImplementedException();
+            int carry = 0;
+            ListNode runner = null;
+            int sumFirst = l1.val + l2.val + carry;
+            if (sumFirst >= 10)
+            {
+                carry = 10;
+            }
+            else
+            {
+                carry = 0;
+            }
+            runner =  new ListNode(sumFirst - carry);//create addition function??? would have to return two things???
+            ListNode headToReturn = runner;
+            carry = carry / 10;
+            
+            while (l1.next != null && l2.next != null)//
+            {
+                l1 = l1.next;
+                l2 = l2.next;
+                int sum = l1.val + l2.val + carry;
+                if(sum >= 10)
+                {
+                    carry = 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+                runner.next = new ListNode(sum - carry);
+                carry = carry / 10;
+                runner = runner.next;
+            } 
+            
+            /*if (l1.next == null && l2.next == null)//once more with feeling
+            {
+                int sum = l1.val + l2.val + carry;
+                if (sum >= 10)
+                {
+                    carry = 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+                runner.next = new ListNode(sum - carry);
+                runner = runner.next;
+                carry = carry / 10;
+                return headToReturn;
+
+            }*/
+            while (l1.next != null || l2.next != null)
+            {
+                if (l1.next != null)//l1 not empty
+                {
+                    l1 = l1.next;
+                    int sum = l1.val + carry;
+                    if (sum >= 10)
+                    {
+                        carry = 10;
+                    }
+                    else
+                    {
+                        carry = 0;
+                    }
+                    runner.next = new ListNode(sum - carry);
+                    runner = runner.next;
+                    carry = carry / 10;
+                }
+                if (l2.next != null)//l2 not empty
+                {
+                    l2 = l2.next;
+                    int sum = l2.val + carry;
+                    if (sum >= 10)
+                    {
+                        carry = 10;
+                    }
+                    else
+                    {
+                        carry = 0;
+                    }
+                    runner.next = new ListNode(sum - carry );
+                    runner = runner.next;
+                    carry = carry / 10;
+                }
+            }
+            if(carry >0)
+            {
+                runner.next = new ListNode(carry);
+            }
+            return headToReturn;
         }
+       
     }
 }
